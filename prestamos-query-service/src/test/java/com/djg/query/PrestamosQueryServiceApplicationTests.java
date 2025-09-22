da99738ec712f75870a176bbe98c0ec37c984ef0
denisjeangop <denisjeangopanchuk@gmail.com>
2025-09-22 21:46:04 +0200

    Add initial project structure with Maven configuration and application files

---
+++ prestamos-query-service/src/test/java/com/djg/query/PrestamosQueryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djg.query;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrestamosQueryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
